594d91e435d38a280997bd02931d9959f2d9d596
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-08-24 08:03:15 +0300

    finish the tabs of all app

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,103 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { icons } from "../../constants";
+import { Tabs } from "expo-router";
 
+const TabIcon = ({ icon, color, name, focused }: any) => {
+  return (
+    <View className="items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+        style={{ color: color }}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>_layout</Text>
-    </View>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: "#FFA001",
+        tabBarInactiveTintColor: "#CDCDE0",
+        tabBarStyle: {
+          backgroundColor: "#161622",
+          borderTopWidth: 1,
+          borderTopColor: "#232533",
+          height: 84,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          headerShown: false,
+          title: "Home",
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabIcon
+              icon={icons.home}
+              color={color}
+              name={"home"}
+              focused={focused}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="bookmark"
+        options={{
+          headerShown: false,
+          title: "Bookmark",
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabIcon
+              icon={icons.bookmark}
+              color={color}
+              name={"Bookmark"}
+              focused={focused}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          headerShown: false,
+          title: "Create",
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabIcon
+              icon={icons.plus}
+              color={color}
+              name={"Create"}
+              focused={focused}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          headerShown: false,
+          title: "Profile",
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabIcon
+              icon={icons.profile}
+              color={color}
+              name={"Profile"}
+              focused={focused}
+            />
+          ),
+        }}
+      />
+    </Tabs>
   );
 };
 
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,15 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import ExpoStatusBar from "expo-status-bar/build/ExpoStatusBar";
+import { Image, StatusBar, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className=" flex-1">
-      <Text className="">Edit app/index.tsx to edit this screen.</Text>
+    <View className=" flex-1 items-center justify-center bg-white">
+      <Text>Aora!</Text>
+      <ExpoStatusBar style="auto" />
+      <Link href={"/home"} className="text-blue-500">
+        Go to Home
+      </Link>
     </View>
   );
 }
