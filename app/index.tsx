0808e0b96d03894fe34d97ebdf2d231b72a2157f
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-08-24 08:28:51 +0300

    initialize the custom button and working on onboardign screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,66 @@
+import { images } from "@/constants";
 import { Link } from "expo-router";
 import ExpoStatusBar from "expo-status-bar/build/ExpoStatusBar";
-import { Image, StatusBar, Text, View } from "react-native";
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  View,
+} from "react-native";
 
-export default function Index() {
+export default function Welcome() {
   return (
-    <View className=" flex-1 items-center justify-center bg-white">
-      <Text>Aora!</Text>
-      <ExpoStatusBar style="auto" />
-      <Link href={"/home"} className="text-blue-500">
-        Go to Home
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      {/* <Loader isLoading={loading} /> */}
+
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full flex justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[298px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless{"\n"}
+              Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless
+            Exploration with Aora
+          </Text>
+
+          {/* <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          /> */}
+        </View>
+      </ScrollView>
+
+      <ExpoStatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
 }
